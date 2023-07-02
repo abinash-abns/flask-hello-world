a1a7c0245193ec9f2258d6165e4542bb48e7e375
Abinash Nayak <65850519+abinash-abns@users.noreply.github.com>
2023-07-02 17:11:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask,request, make_response
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == "GET":
+        response = make_response({"status":"200 OK"}, 200)
+        return response
+    elif request.method == "POST":
+        response = make_response({"status":"200 POST OK"}, 200)
+        return response
